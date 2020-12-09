9a6b94cb4a991af7c49d4c6de21b166af245fad2
seba <Nip14719!>
2020-12-09 22:31:16 +0100

    making a DBSession class

--- db/db_session.py
+++ db/db_session.py
@@ -1,3 +1,23 @@
 from sqlalchemy.orm import sessionmaker
 
+from db.db_engine import DBEngine
+
 Session = sessionmaker()
+
+
+class DBSession(DBEngine):
+
+    def __init__(self, connection_string: str, echo: bool = True):
+        super().__init__(connection_string, echo)
+        Session.configure(bind=self.engine)
+
+        self._session = Session()
+
+    @property
+    def session(self) -> Session:
+        return self._session
+
+    @session.setter
+    def session(self, session: Session):
+        if session:
+            self._session = session
